e3b73f99e78de27f400dd9627bf9ff030d1cdc90
Kayr0r404 <mahlarekarabo.702@gmail.com>
2024-08-05 19:54:49 +0200

    redis

--- utils/redis.js
+++ utils/redis.js
@@ -1,10 +1,11 @@
+// utils/redis.js
+
 import { createClient } from 'redis';
 
 class RedisClient {
     constructor() {
         this.client = createClient();
-        this.client.on('error', err => console.log('Redis Client Error', err.toString()));
-
+        this.client.on('error', (err) => console.log('Redis Client Error', err.toString()));
         this.client.connect();
     }
 
@@ -13,16 +14,29 @@ class RedisClient {
     }
 
     async get(key) {
-        return await this.client.get(key);
+        try {
+            return await this.client.get(key);
+        } catch (err) {
+            console.error('Error getting key from Redis:', err.toString());
+        }
     }
 
     async set(key, value, timeInSec) {
-        await this.client.set(key, value, { EX: timeInSec });
+        try {
+            await this.client.set(key, value, { EX: timeInSec });
+        } catch (err) {
+            console.error('Error setting key in Redis:', err.toString());
+        }
     }
 
     async del(key) {
-        await this.client.del(key);
+        try {
+            await this.client.del(key);
+        } catch (err) {
+            console.error('Error deleting key from Redis:', err.toString());
+        }
     }
 }
 
-export default RedisClient;
+const redisClient = new RedisClient();
+export default redisClient;
