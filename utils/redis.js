2914903205d1d31ae4b443122e9a916e7c0f7b77
Kayr0r404 <mahlarekarabo.702@gmail.com>
2024-08-06 20:00:11 +0200

    use import instead of require

--- utils/redis.js
+++ utils/redis.js
@@ -1,9 +1,9 @@
-const redis = require('redis');
+import { createClient } from 'redis';
 import { promisify } from 'util';
 
 class RedisClient {
   constructor() {
-    this.client = redis.createClient();
+    this.client = createClient();
     this.getAsync = promisify(this.client.get).bind(this.client);
     this.client.on('error', (error) => {
       console.log(`Redis client not connected to the server: ${error.message}`);
