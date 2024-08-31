36c5acdefbbbfafa07fae2bf6b37d3c8f8a7341b
Kayr0r404 <mahlarekarabo.702@gmail.com>
2024-08-31 05:27:46 +0200

    redis engine

--- utils/redis.js
+++ utils/redis.js
@@ -5,11 +5,16 @@ class RedisClient {
   constructor() {
     this.client = createClient();
     this.getAsync = promisify(this.client.get).bind(this.client);
-    this.client.on('error', (error) => {
-      console.log(`Redis client not connected to the server: ${error.message}`);
+    this.setAsync = promisify(this.client.setex).bind(this.client);
+    this.client.on('error', (err) => {
+      console.error('Redis Client Error', err);
     });
   }
 
+  /**
+   * Checks connection to Redis.
+   * @returns true when the connection to Redis is successful, otherwise false.
+   */
   isAlive() {
     return this.client.connected;
   }
@@ -18,15 +23,22 @@ class RedisClient {
     return this.getAsync(key);
   }
 
-  async set(key, value, duration) {
-    this.client.setex(key, duration, value);
+  async set(key, value, timeInSeconds) {
+    try {
+      this.setAsync(key, timeInSeconds, value);
+    } catch (err) {
+      console.error(`Failed to set key ${key}`, err);
+    }
   }
 
   async del(key) {
-    this.client.del(key);
+    try {
+      await this.client.del(key);
+    } catch (err) {
+      console.error(`Failed to delete key ${key}`, err);
+    }
   }
 }
 
 const redisClient = new RedisClient();
-
 export default redisClient;
