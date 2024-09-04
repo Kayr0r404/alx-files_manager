0ddea6f04b9a2cae2bb692f01b57df59b5a09037
Kayr0r404 <mahlarekarabo.702@gmail.com>
2024-09-04 12:21:56 +0200

    use express.json

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-import express from 'express';
+import express, { json } from 'express';
 import routes from './routes/index';
 
 const port = process.env.PORT || 5000;
 const app = express();
 
+app.use(express.json())
 app.use('/', routes);
 
 app.listen(port, () => {
