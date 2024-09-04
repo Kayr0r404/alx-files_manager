5550ceb889c3cde8b101ec464568c4f4571697f1
Kayr0r404 <mahlarekarabo.702@gmail.com>
2024-09-04 02:46:40 +0200

    eslint compliant

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,25 +1,24 @@
-import redisClient from '../utils/redis.js';
-import dbClient from '../utils/db.js';
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
 
 class AppController {
+  static async getStatus(request, response) {
+    const redisStatus = await redisClient.isAlive();
+    const dbStatus = await dbClient.isAlive();
 
-    static async getStatus(request, response) {
-        const redisStatus = await redisClient.isAlive();
-        const dbStatus = await dbClient.isAlive();
-        
-        response.statusCode = 200;
-        response.setHeader('Content-Type', 'application/json');
-        response.end(JSON.stringify({ redis: redisStatus, db: dbStatus }));
-    }
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'application/json');
+    response.end(JSON.stringify({ redis: redisStatus, db: dbStatus }));
+  }
 
-    static async getStats(request, response) {
-        const nbUsers = await dbClient.nbUsers();
-        const nbFiles = await dbClient.nbFiles();
-        
-        response.statusCode = 200;
-        response.setHeader('Content-Type', 'application/json');
-        response.end(JSON.stringify({ users: nbUsers, files: nbFiles }));
-    }
-} 
+  static async getStats(request, response) {
+    const nbUsers = await dbClient.nbUsers();
+    const nbFiles = await dbClient.nbFiles();
+
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'application/json');
+    response.end(JSON.stringify({ users: nbUsers, files: nbFiles }));
+  }
+}
 
 export default AppController;
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import routes from './routes/index.js';
+import routes from './routes/index';
 
 const port = process.env.PORT || 5000;
 const app = express();
@@ -7,7 +7,7 @@ const app = express();
 app.use('/', routes);
 
 app.listen(port, () => {
-    console.log('web server listening on http://localhost:1245');
-  });
-  
-  export default app;
+  console.log('web server listening on http://localhost:1245');
+});
+
+export default app;
