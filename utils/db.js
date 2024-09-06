13e67a000dc9b597a20aeae23c8d19ce21c26df3
Kayr0r404 <mahlarekarabo.702@gmail.com>
2024-09-06 04:01:48 +0200

    refactored mongon engine

--- utils/db.js
+++ utils/db.js
@@ -1,4 +1,6 @@
-import { MongoClient } from 'mongodb';
+import pkg from 'mongodb';
+
+const { MongoClient } = pkg;
 
 const host = process.env.DB_HOST || 'localhost';
 const port = process.env.DB_PORT || 27017;
@@ -8,12 +10,28 @@ const url = `mongodb://${host}:${port}/`;
 class DBClient {
   constructor() {
     this.db = null;
-    MongoClient.connect(url, { useUnifiedTopology: true }, (error, client) => {
-      if (error) console.log(error);
+    this.init();
+  }
+
+  async init() {
+    try {
+      const client = await MongoClient.connect(url, { useUnifiedTopology: true });
       this.db = client.db(database);
-      this.db.createCollection('users');
-      this.db.createCollection('files');
-    });
+
+      const userCollections = await this.db.listCollections({ name: 'users' }).toArray();
+      if (userCollections.length === 0) {
+        await this.db.createCollection('users');
+        console.log('Created "users" collection');
+      }
+
+      const fileCollections = await this.db.listCollections({ name: 'files' }).toArray();
+      if (fileCollections.length === 0) {
+        await this.db.createCollection('files');
+        console.log('Created "files" collection');
+      }
+    } catch (error) {
+      console.error('Error connecting to MongoDB:', error);
+    }
   }
 
   isAlive() {
@@ -29,6 +47,10 @@ class DBClient {
     const user = await this.db.collection('users').findOne(query);
     console.log('GET USER IN DB.JS', user);
     return user;
+  }
+
+  async insertUser(query) {
+    const user = await this.db.collection('user').insertOne(query);
   }
 
   async nbFiles() {
